c9c31b90f35918594502d732b194cf612e3e5064
Kojo Akyer <kojoakyer777@gmail.com>
2021-10-09 16:17:58 +0100

    initial commit

---
+++ src/components/HeaderSection/ImageData.jsx
@@ -0,0 +1,47 @@
+export const imageData =[
+    {
+        image:'assets/img1.jpg',
+        title:'USHERING',
+        Descr:'Quality Assured',
+        children:'JOIN US'
+    },
+    {
+        image:'assets/img2.jpg',
+        title:'Event Setup',
+        Descr:'Satisfaction Guranteed',
+        children:'GET STARTED'
+    },
+    {
+        image:'assets/img3.jpg',
+        title:'Product Activation',
+        Descr:'Exceptional Service',
+        children:'FOLLOW'
+    },
+    {
+        image:'assets/img3.jpg',
+        title:'PhotoShoot and Commercials',
+        Descr:'Quality Assured',
+        children:'SIGN UP'
+    },
+    {
+        image:'assets/img4.jpg',
+        title:'Event Hosting',
+        Descr:'Excellence our Hallmark',
+        children:'BOOK'
+    },
+    {
+        image:'assets/img5.jpg',
+        title:'Labbele Event and Ushering Agency',
+        Descr:'We are Happy to Serve',
+        children:'SIGN IN'
+    },
+    {
+        image:'assets/img6.jpg',
+        title:'Book Us Now',
+        Descr:'The name synanymous with success',
+        children:'SOCIALS'
+    },
+
+
+
+]
